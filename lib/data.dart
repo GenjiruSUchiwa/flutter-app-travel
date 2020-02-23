8c1694fe6b37532b2e661ed8b28330b05671b141
GenjiruSUchiwa <genjiru23@gmail.com>
2020-02-23 16:56:24 +0100

    Final commit to my First Flutter App design

---
+++ lib/data.dart
@@ -0,0 +1,13 @@
+List<String> images = [
+  "assets/image_04.jpg",
+  "assets/image_03.jpg",
+  "assets/image_02.jpg",
+  "assets/image_01.jpg"
+];
+
+List<String> title = [
+  "The Winter Girl",
+  "House in hats",
+  "Bad luck is orange",
+  "Heart Soul",
+];
